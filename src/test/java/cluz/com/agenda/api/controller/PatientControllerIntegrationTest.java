4eda601ef9657b72bb26e818967acd77d617b390
charlesluz1994 <charlesluz.94@gmail.com>
2023-07-08 07:51:19 +0100

    Inserting system.properties to cloud provider.
    Update integration tests with @tag
    @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
    
    This tag ensures that the context is considered "dirty" before each test method is executed. This ensures that the database is deleted and recreated before each test is run.

--- src/test/java/cluz/com/agenda/api/controller/PatientControllerIntegrationTest.java
+++ src/test/java/cluz/com/agenda/api/controller/PatientControllerIntegrationTest.java
@@ -5,14 +5,12 @@ import cluz.com.agenda.domain.entity.Patient;
 import cluz.com.agenda.domain.repository.PatientRepository;
 import cluz.com.agenda.exception.BusinessException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -23,8 +21,9 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 @SpringBootTest
-@AutoConfigureMockMvc(addFilters = false) //exclude security part of requests.
-class PatientControllerTest {
+@AutoConfigureMockMvc(addFilters = false)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+class PatientControllerIntegrationTest {
 
     @Autowired
     MockMvc mockMvc;
@@ -45,10 +44,10 @@ class PatientControllerTest {
         repository.save(patient);
     }
 
-//    @AfterAll
-//    void down() {
-//        repository.deleteAll();
-//    }
+    @AfterEach
+    void down() {
+        repository.deleteAllInBatch();
+    }
 
     @Test
     @DisplayName("Given a patient when save patient then return a 200 status code")
@@ -69,7 +68,7 @@ class PatientControllerTest {
 
     @Test
     @DisplayName("given a request when find all patients then return 200 status code")
-    void given_AnID_when_FindAllPatients_then_Return200StatusCode() throws Exception {
+    void given_ID_when_FindAllPatients_then_Return200StatusCode() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/patient"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andDo(print());
@@ -77,7 +76,7 @@ class PatientControllerTest {
 
     @Test
     @DisplayName("Given an ID when find patient ByID then return a 200 status code")
-    void given_AnID_when_FindPatientByID_then_Return200StatusCode() throws Exception {
+    void given_ID_when_FindPatientByID_then_Return200StatusCode() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/patient/1"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andDo(print());
@@ -85,11 +84,11 @@ class PatientControllerTest {
 
     @Test
     @DisplayName("Given an ID when update patient request then return 200 status code")
-    void given_AnID_when_UpdatePatient_then_Return200StatusCode() throws Exception {
+    void given_ID_when_UpdatePatient_then_Return200StatusCode() throws Exception {
         PatientRequest patient = PatientRequest.builder()
-                .name("Charles")
-                .lastname("Luz")
-                .cpf("00100200388")
+                .name("Charlie")
+                .lastname("Santos")
+                .cpf("418292378")
                 .email("charles@gmail.com").build();
 
         mockMvc.perform(MockMvcRequestBuilders.put("/patient/1")
@@ -102,13 +101,7 @@ class PatientControllerTest {
 
     @Test
     @DisplayName("Given an ID when delete patient then return 204 status code")
-    void given_AnID_when_DeletePatient_then_Return204StatusCode() throws Exception {
-        PatientRequest patient = PatientRequest.builder()
-                .name("Charles")
-                .lastname("Luz")
-                .cpf("00100200388")
-                .email("charles@gmail.com").build();
-
+    void given_ID_when_DeletePatient_then_Return204StatusCode() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.delete("/patient/1"))
                 .andExpect(MockMvcResultMatchers.status().isNoContent())
                 .andDo(print());
@@ -116,13 +109,13 @@ class PatientControllerTest {
     }
 
     @Test
-    @DisplayName("Given a patient when post request then throw an exception")
+    @DisplayName("Given a patient when save with patient already existence then throw an exception")
     void given_Patient_when_SavePatientWithExistingCPF_then_ThrowAnException() throws Exception {
         PatientRequest patient = PatientRequest.builder()
-                .name("Nathy")
+                .name("Charles")
                 .lastname("Luz")
                 .cpf("00100200388")
-                .email("nathy@gmail.com").build();
+                .email("charles@gmail.com").build();
 
         mockMvc.perform(post("/patient")
                         .contentType(MediaType.APPLICATION_JSON)
